8b1837edc12aa598165e1683857bdc2e1d6e9c72
sorayaskavinski <soraya1br@gmail.com>
2024-11-09 15:48:15 -0800

    ..

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 const yearElement = document.querySelector("#year");
 yearElement.textContent = new Date().getFullYear();
 const lastModifiedElement = document.querySelector("#lastModified");
-lastModifiedElement.textContent += document.lastModified;
+lastModifiedElement.textContent = "Last updated: " + document.lastModified;
