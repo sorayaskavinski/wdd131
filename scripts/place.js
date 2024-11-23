8d2f6308d6dea11f044f312ef16b8a6cdff5b9a0
sorayaskavinski <soraya1br@gmail.com>
2024-11-23 14:08:33 -0800

    ..

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,3 @@
-// Function to calculate Wind Chill
 function calculateWindChill(temp, windSpeed) {
     if (temp <= 10 && windSpeed > 4.8) {
         return Math.round(13.12 + 0.6215 * temp -  11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16));
