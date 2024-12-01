b7bfdf0d08e18bffb85e7b4efb1d5356173fc096
sorayaskavinski <soraya1br@gmail.com>
2024-11-30 16:57:36 -0800

    ..

--- scripts/place.js
+++ scripts/place.js
@@ -1,11 +1,10 @@
 function calculateWindChill(temp, windSpeed) {
     if (temp <= 10 && windSpeed > 4.8) {
-        return Math.round(13.12 + 0.6215 * temp -  11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16));
+        return Math.round(13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16));
     } else {
-        return 'N/A'; 
+        return 'N/A';
     }
 }
-
 
 window.addEventListener('DOMContentLoaded', (event) => {
     const temp = 20; 
