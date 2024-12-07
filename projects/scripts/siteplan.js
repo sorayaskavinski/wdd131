7d904cabbeb39bf6479045a973cd290a4872fa47
sorayaskavinski <soraya1br@gmail.com>
2024-12-07 18:29:58 -0300

    ...

--- projects/scripts/siteplan.js
+++ projects/scripts/siteplan.js
@@ -1,4 +1,18 @@
-const yearElement = document.querySelector("#year");
-yearElement.textContent = new Date().getFullYear();
-const lastModifiedElement = document.querySelector("#lastModified");
-lastModifiedElement.textContent = "Last updated: " + document.lastModified;
+document.getElementById('year').textContent = new Date().getFullYear();
+
+const lastModifiedDate = new Date(document.lastModified);
+document.getElementById('lastModified').textContent =
+    `Last Updated: ${lastModifiedDate.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    })}`;
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    const isVisible = navMenu.classList.toggle('visible');
+    hamburger.setAttribute('aria-expanded', isVisible);
+    hamburger.textContent = isVisible ? '✖' : '☰';
+});
