3e8e128f71f5127e5c1d09ea524f7703c8e8c521
sorayaskavinski <soraya1br@gmail.com>
2024-12-13 17:05:37 -0300

    ..

--- projects/scripts/siteplan.js
+++ projects/scripts/siteplan.js
@@ -1,13 +1,5 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
-const lastModifiedDate = new Date(document.lastModified);
-document.getElementById('lastModified').textContent =
-    `Last Updated: ${lastModifiedDate.toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    })}`;
-
 document.getElementById("hamburger").addEventListener("click", function() {
     const nav = document.querySelector("nav ul");
     nav.classList.toggle("visible");
