573603e9a22458a5f7e78f6671ce2dc9c88f0820
sorayaskavinski <soraya1br@gmail.com>
2024-12-13 18:22:30 -0300

    ...

--- projects/scripts/form-data.js
+++ projects/scripts/form-data.js
@@ -1,5 +1,22 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
+const lastModifiedDate = new Date(document.lastModified);
+document.getElementById('lastModified').textContent =
+    `Last Updated: ${lastModifiedDate.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    })}`;
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    const isVisible = navMenu.classList.toggle('visible');
+    hamburger.setAttribute('aria-expanded', isVisible);
+    hamburger.textContent = isVisible ? 'X' : '☰';
+});
+
 const formData = JSON.parse(localStorage.getItem('formData'));
 if (formData) {
     document.getElementById('name').textContent = formData.name;
